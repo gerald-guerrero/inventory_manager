eb86bb3c86f057d8251ec6081efb7ea4a83a595c
maliqharris <maliqharris123@gmail.com>
2025-01-06 13:59:29 -0500

    Added Psuedo-Code

---
+++ inventory_operations.py
@@ -0,0 +1,27 @@
+#Add item
+
+#Add item name , item_count = 1
+#return(bool)
+#if bool = True:
+    #print("Success")
+    #else:
+        #print("Failure")
+
+#Remove Item
+
+#if item is in list:
+    #item_count +-1
+    #else:
+        #return(False) 
+        #print(No item found!)
+
+
+#Update quantity
+
+#take in item_name, new_quant 
+
+#item_count = new_quant
+
+#if item_name is not in list:
+    #Add item
+    #print(f"Item added with a quantity or {item_count}")
