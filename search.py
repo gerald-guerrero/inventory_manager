9915109806fe9b1b367de88da162b1774ad2f3fa
Avery <avery_keyes@yahoo.com>
2025-01-06 14:29:06 -0500

    Psuedo Code

---
+++ search.py
@@ -0,0 +1,11 @@
+#  Create search_item() function
+
+# def search_item(user_provided_inventory, "user_search_key")
+    if user_search_key.lower() in user_provided_inventory.lower():
+        # it's a match
+        # Print "user_search_key" found.
+        # Print found dict item
+    else:
+        # print "Item not found"
+         
+# search_item(inventory, "ap") might find "Apple" and "Paper"
