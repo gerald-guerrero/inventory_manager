42ae7785fa947db2e808015d5028176dab8bd695
Sierra <sierraashford@gmail.com>
2025-01-06 14:12:14 -0500

    User Input

--- user_input.py
+++ user_input.py
@@ -1,1 +1,11 @@
-#Module A 
+#Get the Menu Choice function ask the user to choose a menu option.
+def get_main_menu_choice():
+    return
+
+#Get Item Details ask the user for an item's name and quantity.
+def get_item_details():
+    return
+
+#Get a Search Term ask the user for a search term.
+def get_search_term():
+    return
