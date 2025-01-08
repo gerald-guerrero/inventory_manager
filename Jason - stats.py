487d66d7bbd05be43594aaa4a7e13a9c23c65412
Jason <jasonwilliams365@yahoo.com>
2025-01-08 13:44:55 -0500

    Updated commit

--- Jason - stats.py
+++ Jason - stats.py
@@ -6,16 +6,16 @@ def get_total_quantity(inventory):   #adds numeric values in inventory and gives
     return sum(inventory.values())
 
 def get_top_item(inventory):   #get the top item (highest number) in the inventory
-    try:
+    try:                       #Try/except block should only work if numbers are not a tie
         return max(inventory, key=inventory.get)
     except ValueError:
         return None
     
-# if __name__ == "__main__":
-#       mock_inv = {"Apple": 10, "Orange": 5}
-#       print(get_inventory_count(mock_inv))  # Expect 2
-#       print(get_total_quantity(mock_inv))   # Expect 15
-#       print(get_top_item(mock_inv))         # Expect "Apple"
+if __name__ == "__main__":
+      mock_inv = {"Apple": 6, "Orange": 5}
+      print(get_inventory_count(mock_inv))  # Expect 2
+      print(get_total_quantity(mock_inv))   # Expect 15
+      print(get_top_item(mock_inv))         # Expect "Apple"
 
 
 
