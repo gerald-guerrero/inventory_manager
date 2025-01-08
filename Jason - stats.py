d50b508a135fd7753ad45c2fbc428a4126bd2641
Jason <jasonwilliams365@yahoo.com>
2025-01-08 13:38:06 -0500

    Updated code

--- Jason - stats.py
+++ Jason - stats.py
@@ -1,18 +1,21 @@
-def get_inventory_item():   #get the number of items in the inventory
-    return len()
 
-def get_inventory_total():   #get the total number of items quantities
-    return sum()
+def get_inventory_count(inventory):   #counts number of items in the inventory
+    return len(inventory)
 
+def get_total_quantity(inventory):   #adds numeric values in inventory and gives sum
+    return sum(inventory.values())
 
-try:
-    def get_inventory_top_item():   #get the top item in the inventory
-        return max()
-except:
-    print("None")
-
-
-
+def get_top_item(inventory):   #get the top item (highest number) in the inventory
+    try:
+        return max(inventory, key=inventory.get)
+    except ValueError:
+        return None
+    
+# if __name__ == "__main__":
+#       mock_inv = {"Apple": 10, "Orange": 5}
+#       print(get_inventory_count(mock_inv))  # Expect 2
+#       print(get_total_quantity(mock_inv))   # Expect 15
+#       print(get_top_item(mock_inv))         # Expect "Apple"
 
 
 
