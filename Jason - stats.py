159e177c7c500b20cf377ebcfaa030e7dfcb1a47
Jason <jasonwilliams365@yahoo.com>
2025-01-08 00:10:53 -0500

    Psuedo code

--- Jason - stats.py
+++ Jason - stats.py
@@ -1,21 +1,18 @@
-inv = {'apples': 5, 'oranges': 10, 'pears': 15}
+def get_inventory_item():   #get the number of items in the inventory
+    return len()
+
+def get_inventory_total():   #get the total number of items quantities
+    return sum()
 
 
-#Return how many **distinct items** are in the inventory (e.g., `len(inventory)` if it’s a dict).
-get_inventory_count = len(inv)
+try:
+    def get_inventory_top_item():   #get the top item in the inventory
+        return max()
+except:
+    print("None")
 
 
-#Sum all item quantities.
-get_total_quantity = sum
 
 
-#Return the **item with the highest quantity**.  
-#Handle ties or empty inventory as you see fit (maybe return `None` or an empty string in those cases).
-get_top_item = max
-if __name__ == None:
-    print('Please enter item: ')
-     
 
-    print(get_inventory_count(inv))
-    print(get_total_quantity(inv))
-    print(get_top_item(inv))
+
