d13d1b5ec134231bb5614b885ae3f8ac3eec9dce
maliqharris <maliqharris123@gmail.com>
2025-01-08 10:29:44 -0500

    Finished add_item function, added remove_item, and update quantity function, waiting for validator.py to import validations

--- inventory_operations.py
+++ inventory_operations.py
@@ -4,65 +4,73 @@
 inventory = []
 #Add item
 def add_item(inventory, item_name, quantity):
-    
+
     #Get rid of spaces 
     fruit_no_spaces = str(item_name).strip().upper()
-    
+
     #If fruit exists in inventory then add to quantity 
     for item in inventory:
         if fruit_no_spaces in item:
             print(f"Item found adding to quantity!")
             print(f"Updated quantity from {item[fruit_no_spaces]} to {quantity + item[fruit_no_spaces]}")
+
             #add item quantitiy to existing quantity - quantity is added to the quantity attatched to the found key 
             item[fruit_no_spaces] += quantity
-            
+            print("Status: Success!")
             return True
     
-    # Asigns value to key of fruit - asigns quantity to new fruit 
+    # If not found Asigns value to key of fruit - asigns quantity to new fruit 
     new_fruit = {fruit_no_spaces: int(quantity)}  
     inventory.append(new_fruit)
     print(f"Created new item! {fruit_no_spaces} added to inventory") 
-     
+    print("Status: Success!")
     return True
 
-#make status to be false if error. 
 
- 
-    
-add_item(inventory, "apple", 4)
-add_item(inventory, "Apple", 4)
-add_item(inventory, "    Apple", 5)
-# add_item(inventory, "Banana", 8)
-# add_item(inventory, "Mango", 10)
-# add_item(inventory,"cantlope", 8)
-# add_item(inventory, "Pineapple", 10)
 
-print(inventory)
+
    
     
-
-#return(bool)
-#if bool = True:
-    #print("Success")
-    #else:
-        #print("Failure")
-#Add item to list !
-
 #Remove Item
 
-#if item is in list:
-    #item_count +-1
-    #else:
-        #return(False) 
-        #print(No item found!)
+def remove_item(inventory, item_name):
+    #converts name 
+    fruit_no_spaces = str(item_name).strip().upper()
+    #if item in list
+    for item in inventory:
+        if fruit_no_spaces in item:
+            inventory.remove(item)
+            print("item found! Deleting item")
+            return True
+    else:
+        print("Fruit not found!")
+        return False
+        
 
 
 #Update quantity
 
-#take in item_name, new_quant 
+def update_quantity(inventory, item_name, new_quantity):
 
-#item_count = new_quant
+    fruit_no_spaces = str(item_name).strip().upper()
+    
+    for item in inventory:
+        if fruit_no_spaces in item:
+            item[fruit_no_spaces] = new_quantity
+            print("Item found. Updating quantity")
+            return True
+    else:    
+        print("Item not found, adding item")
+        add_item(inventory, item_name, new_quantity )
+        return True
 
-#if item_name is not in list:
-    #Add item
-    #print(f"Item added with a quantity or {item_count}")
+# add_item(inventory, "Apple", 8)
+
+# print(inventory)
+# remove_item(inventory, "  apple")
+# print(inventory)
+# update_quantity(inventory, "apple", 4)
+# print(inventory)
+
+
+\
