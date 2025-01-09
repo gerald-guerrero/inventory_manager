276bda161c52a9708998206d4ec575062d66a7a8
Jason <jasonwilliams365@yahoo.com>
2025-01-09 10:35:03 -0500

    Code now accepts list of dicts

--- Jason_stats.py
+++ Jason_stats.py
@@ -1,21 +1,27 @@
+def get_inventory_count(inventory_list):   # counts number of items in the inventory list
+    return len(inventory_list)
 
-def get_inventory_count(inventory):   #counts number of items in the inventory
-    return len(inventory)
+def get_total_quantity(inventory_list):   # adds numeric values in inventory list and gives sum
+    total_quantity = 0
+    for inventory in inventory_list:
+        total_quantity += sum(inventory.values())
+    return total_quantity
 
-def get_total_quantity(inventory):   #adds numeric values in inventory and gives sum
-    return sum(inventory.values())
-
-def get_top_item(inventory):   #get the top item (highest number) in the inventory
-    try:                       #Try/except block should only work if numbers are not a tie
-        return max(inventory, key=inventory.get)
+def get_top_item(inventory_list):   # get the top item (highest number) in the inventory list
+    combined_inventory = {}
+    for inventory in inventory_list:
+        for item, quantity in inventory.items():
+            if item in combined_inventory:
+                combined_inventory[item] += quantity
+            else:
+                combined_inventory[item] = quantity
+    try:                       # Try/except block should only work if numbers are not a tie
+        return max(combined_inventory, key=combined_inventory.get)
     except ValueError:
         return None
     
 if __name__ == "__main__":
-      mock_inv = {"Apple": 6, "Orange": 5}
-      print(get_inventory_count(mock_inv))  # Expect 2
-      print(get_total_quantity(mock_inv))   # Expect 15
-      print(get_top_item(mock_inv))         # Expect "Apple"
-
-
-
+    mock_inv = [{"Apple": 6}, {"Orange": 6}]
+    print(get_inventory_count(mock_inv))  # Expect 2
+    print(get_total_quantity(mock_inv))   # Expect 12
+    print(get_top_item(mock_inv))         # Expect "None"
